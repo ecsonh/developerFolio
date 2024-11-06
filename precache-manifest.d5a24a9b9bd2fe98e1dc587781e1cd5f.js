aa8387c5c8be61390e6462b3286b84a0b8dca039
Ecson Hsu <ecsonh@uci.edu>
2024-11-05 16:26:19 -0800

    Updates

---
+++ precache-manifest.d5a24a9b9bd2fe98e1dc587781e1cd5f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e089a2611da2bd4f501bdb85b82600",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "be468c409b8e580e4e69",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "2823b77d38a280ac5f01",
+    "url": "/developerFolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "583a6d57f2081ad0d118",
+    "url": "/developerFolio/static/css/main.a89ed154.chunk.css"
+  },
+  {
+    "revision": "e956537ae28641a4196a",
+    "url": "/developerFolio/static/js/2.434e1995.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/developerFolio/static/js/2.434e1995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be468c409b8e580e4e69",
+    "url": "/developerFolio/static/js/3.5345dcc9.chunk.js"
+  },
+  {
+    "revision": "2823b77d38a280ac5f01",
+    "url": "/developerFolio/static/js/4.281e94b1.chunk.js"
+  },
+  {
+    "revision": "583a6d57f2081ad0d118",
+    "url": "/developerFolio/static/js/main.609df265.chunk.js"
+  },
+  {
+    "revision": "a6d68423f4907b4e5b9b",
+    "url": "/developerFolio/static/js/runtime-main.dd91de17.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5843e423072a4d1004b9767b727f7334",
+    "url": "/developerFolio/static/media/ai bot.5843e423.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ec8136549cfc7417f67c626c79d785d8",
+    "url": "/developerFolio/static/media/resume.ec813654.pdf"
+  },
+  {
+    "revision": "c55214837f16325c01a72a4755fd68ab",
+    "url": "/developerFolio/static/media/rexgear.c5521483.png"
+  },
+  {
+    "revision": "1e0e3e073d6c76ccc0f234df6d11766e",
+    "url": "/developerFolio/static/media/shopping platform.1e0e3e07.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "f34f9dc805bfaa303b6be83b8de018a5",
+    "url": "/developerFolio/static/media/uci.f34f9dc8.png"
+  }
+]);
